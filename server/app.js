b25e41715f2f3e7867fff9857438cb71f4ada420
meoki <1084415961@qq.com>
2021-04-12 17:27:42 +0800

    添加token验证 和全局主路由的拦截

--- server/app.js
+++ server/app.js
@@ -1,14 +1,41 @@
 const express = require("express");
 const app = new express();
 const bodyParser = require('body-parser')
-
+const jwt = require('jsonwebtoken')
 const main = require('./router/main')
 
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
-app.use('/api', main)
 const port = 9000; // 端口号
+
+// 验证客户端是否传回token
+//拦截所有主路由
+app.use('/api',(req,res,next)=>{
+    // 当请求接口是登录或者注册接口时候  放行
+    const token = String(req.headers.authorization)
+    const username = jwt.decode(token, 'meoki')
+    if(req.url == '/login' || req.url == '/register'){
+        next()
+        return 
+    }
+    //验证token
+    // 判断客户端是否传递或者解密token  如果没有传递或者没有进行解密 或者解密失败，错误信息
+    if(token == undefined || username == null){
+        res.send({
+            code: 9999,
+            data: null,
+            meta:{
+                msg: 'token无效'
+            }
+        })
+        return 
+    } 
+    next()
+})
+
+app.use('/api', main)
+
 
 app.listen(port, () => {
   console.log(`=======在${port}上启动成功`);
--- server/router/main.js
+++ server/router/main.js
@@ -9,4 +9,8 @@ const main = express.Router()
 main.post('/register', require('./user/register'))
 
 main.post('/login' ,require('./user/login'))
+
+main.get('/test', (req,res)=>{
+    res.send('ok')
+})
 module.exports = main
