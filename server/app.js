c14e74aada09491dcd6e74207584175970a7f01e
meoki <1084415961@qq.com>
2021-04-13 16:43:21 +0800

    连接数据库

--- server/app.js
+++ server/app.js
@@ -1,8 +1,31 @@
 const express = require("express");
+const mysql = require('mysql')
 const app = new express();
 const bodyParser = require('body-parser')
 const jwt = require('jsonwebtoken')
 const main = require('./router/main')
+// 配置跨域----->也可以通过服务端安装配置CORS
+app.use((req, res, next)=> {
+    res.header("Access-Control-Allow-Origin", req.headers.origin); // 设置允许来自哪里的跨域请求访问（值为*代表允许任何跨域请求，但是没有安全保证）
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS"); // 设置允许接收的请求类型
+    res.header("Access-Control-Allow-Headers", "Content-Type,request-origin"); // 设置请求头中允许携带的参数
+    res.header("Access-Control-Allow-Credentials", "true"); // 允许客户端携带证书式访问。保持跨域请求中的Cookie。注意：此处设true时，Access-Control-Allow-Origin的值不能为 '*'
+    res.header("Access-control-max-age", 1000); // 设置请求通过预检后多少时间内不再检验，减少预请求发送次数
+    next();
+})
+
+// 连接数据库
+const dbServer = mysql.createConnection({
+    host: 'localhost',
+    port: 3306, // 默认3306
+    user: 'root',
+    password: '123456',
+    database: 'mydatabase' // 创建的数据库
+})
+dbServer.connect((err)=>{
+    if(err) {throw err}
+    console.log('数据库连接成功')
+})
 
 
 app.use(bodyParser.urlencoded({extended: false}))
